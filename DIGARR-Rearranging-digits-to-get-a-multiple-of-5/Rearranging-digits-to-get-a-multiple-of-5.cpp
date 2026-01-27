faea5180152ae9453958aee29097abd33d806674
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-01-28 00:15:50 +0530

    Solved: Rearranging digits to get a multiple of 5 (DIGARR)

---
+++ DIGARR-Rearranging-digits-to-get-a-multiple-of-5/Rearranging-digits-to-get-a-multiple-of-5.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,num;
+        cin >> n >> num;
+        bool found = false;
+        while(num!=0)
+        {
+            int re = num%10;
+            if(re ==5 || re == 0)
+            {
+                found = true;
+            }
+            num = num/10;
+        }
+        if(found)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+}
