d3ee563a6d1558e10b1a2c90ada7974548e8db4a
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-11 20:08:47 +0530

    Solved: Payment Scheme (PAYSC)

---
+++ PAYSC-Payment-Scheme/Payment-Scheme.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a;
+    cin >> a;
+    int fir = 100 + a*4;
+    cout << min(fir,300) << endl;
+}
