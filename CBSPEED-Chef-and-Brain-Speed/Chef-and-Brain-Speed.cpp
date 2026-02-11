6dd6e969838817e20ac81fa73f0f235ebb8a79bc
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-11 21:39:25 +0530

    Solved: Chef and Brain Speed (CBSPEED)

---
+++ CBSPEED-Chef-and-Brain-Speed/Chef-and-Brain-Speed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    // your code goes here
+    if(y>x)
+    {
+        cout << "Yes" ;
+    }
+    else
+    {
+        cout << "NO" ;
+    }
+    return 0;
+}
