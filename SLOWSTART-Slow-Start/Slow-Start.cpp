4a62c2b30d7936faee3cdf1daa4978123681f4a1
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-12 14:28:33 +0530

    Solved: Slow Start (SLOWSTART)

--- SLOWSTART-Slow-Start/Slow-Start.cpp
+++ SLOWSTART-Slow-Start/Slow-Start.cpp
@@ -1,24 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
+            cout << ceil((float)c/a)+5 << endl;
 
-int main() {
-    // your code goes here
-    int t;
-    cin >> t;
-    while(t--)
-    {
-        int a,b;
-        cin >> a >> b;
-        int ter = a/2;
-        int maxa = ter*5;
-        if(maxa >= b)
-        {
-            cout << ceil(b/ter)+1 << endl;
-        }
-        else
-        {
-            int c = b-maxa;
-            cout << ceil(c/a) + 6 << endl;
-        }
-    }
-}
