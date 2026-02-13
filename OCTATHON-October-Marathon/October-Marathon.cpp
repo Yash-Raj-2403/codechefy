ade275b2be06a412a0169c3ebc67a4a1fa70c857
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-13 23:23:12 +0530

    Solved: October Marathon (OCTATHON)

---
+++ OCTATHON-October-Marathon/October-Marathon.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a;
+    cin >> a;
+    if(a<3)
+    {
+        cout << "GOLD" << endl;
+    }
+    else if(a<6)
+    {
+        cout << "SILVER" << endl;
+    }
+    else
+    {
+        cout << "BRONZE" << endl;
+    }
+}
