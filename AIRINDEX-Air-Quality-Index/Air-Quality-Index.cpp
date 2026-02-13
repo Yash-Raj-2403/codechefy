55172952f0ebea4ae7fdfd9b81fbab1d9ab306c7
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-13 23:17:51 +0530

    Solved: Air Quality Index (AIRINDEX)

---
+++ AIRINDEX-Air-Quality-Index/Air-Quality-Index.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a;
+    cin >> a;
+    if(a<100)
+    {
+        cout << "YES" ;
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
