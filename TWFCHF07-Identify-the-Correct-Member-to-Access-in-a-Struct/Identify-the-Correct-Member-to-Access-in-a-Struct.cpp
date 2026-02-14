b669b9b11c06744c2fca0a25d652a9ab4b0fbc10
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-14 23:09:27 +0530

    Solved: Identify the Correct Member to Access in a Struct (TWFCHF07)

---
+++ TWFCHF07-Identify-the-Correct-Member-to-Access-in-a-Struct/Identify-the-Correct-Member-to-Access-in-a-Struct.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Define a structure to represent a device
+struct Device {
+    char name[30];
+    int year;
+};
+
+int main() {
+    // Declare and initialize a structure variable
+    struct Device d1 = {"Thermal Scanner", 2021};
+    printf("Structure declared and initialized successfully.");
+    return 0;
+}
