139f534b702291d46df975193f9c33157ecc0162
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-15 17:30:23 +0530

    Solved: Rearrange Code (JPRACREARR1)

---
+++ JPRACREARR1-Rearrange-Code/Rearrange-Code.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt(), b = read.nextInt();
+	int c = a + b;
+    	System.out.print(c);
+    }
+}
