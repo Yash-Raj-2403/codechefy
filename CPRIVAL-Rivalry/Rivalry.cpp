873f557348dc5f2857b7ff6c70eb29eb06e0c1b7
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-17 22:52:28 +0530

    Solved: Rivalry (CPRIVAL)

---
+++ CPRIVAL-Rivalry/Rivalry.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int x=a+c;
+    int y=b+d;
+    if(x>y)
+    cout<<"Dominater"<<endl;
+    else
+        cout<<"Everule"<<endl;
+    
+    // your code goes here
+    return 0;
+}
