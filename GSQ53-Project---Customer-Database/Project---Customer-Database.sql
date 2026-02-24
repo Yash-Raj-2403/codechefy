26c224c117c1b252d3c6bf6a287a1711c807db1c
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-24 23:06:57 +0530

    Solved: Project - Customer Database (GSQ53)

---
+++ GSQ53-Project---Customer-Database/Project---Customer-Database.sql
@@ -0,0 +1,23 @@
+/* Write a query which does the following */
+
+-- Create a table 'customer'
+create table customer(
+Id INT PRIMARY KEY,
+Name Text,
+Age INT,
+address Text
+);
+
+
+
+
+
+-- Insert data into the table 
+
+VALUES(1, 'John Smith', 25,  '123 Main St'),
+(2, 'Sarah Johnson', 30,'456 Broadway'),
+(3, 'Michael Brown', 45, '789 5th Ave'),
+(4, 'Jessica Davis', 28, '321 Elm St'),
+(5, 'David Lee', 35,  '555 Park Ave');
+
+-- Output the entire table
