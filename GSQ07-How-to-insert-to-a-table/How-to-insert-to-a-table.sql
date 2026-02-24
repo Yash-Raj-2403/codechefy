e5e9d112d08102650cc632ce8d2c63462201b226
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-24 22:47:21 +0530

    Solved: How to insert to a table (GSQ07)

---
+++ GSQ07-How-to-insert-to-a-table/How-to-insert-to-a-table.sql
@@ -0,0 +1,5 @@
+/* Write a query to add the below mentioned employee details to the EMPLOYEE table.
+Employee id: 06, Employee Name: Brandon Kim, Department: Operations
+*/
+insert into employee(Employee_id,Employee_Name,Department)
+values(6,'Brandon Kim','Operations');
