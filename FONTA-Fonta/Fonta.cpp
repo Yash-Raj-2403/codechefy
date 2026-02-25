ce8700a497994a354b6fe6a75734049eaddef601
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-25 20:02:35 +0530

    Solved: Fonta (FONTA)

---
+++ FONTA-Fonta/Fonta.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    string s;
+    cin >> s;
+    if(s[2] == 'n' && s[3] == 't' && s[4] == 'a')
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+}
