35761d0924c0b1d9cc4c766a7d6f61142c796fa1
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-03-02 12:28:27 +0530

    Solved: Chef and Spells (CHFSPL)

---
+++ CHFSPL-Chef-and-Spells/Chef-and-Spells.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        vector<int> a(3);
+        for(int i=0;i<3;i++)
+        {
+            cin >> a[i];
+        }
+        sort(a.begin(),a.end());
+        cout << a[1] + a[2] << endl;
+    }
+}
