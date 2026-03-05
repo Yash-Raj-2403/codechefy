8f00da551aea132ce6253c766648ef022e0f43cb
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-03-05 23:13:35 +0530

    Solved: TCS Examination (EXAMTIME)

---
+++ EXAMTIME-TCS-Examination/TCS-Examination.cpp
@@ -0,0 +1,28 @@
+                            cout<<"DRAGON"<<endl;
+                        }
+                        else if(a[1]<b[1])
+                        {
+                            cout<<"SLOTH"<<endl;
+                        }
+                        else
+                        {
+                                if(a[2]>b[2])
+                                {
+                                    cout<<"DRAGON"<<endl;
+                                }
+                                else if(a[2]<b[2])
+                                {
+                                    cout<<"SLOTH"<<endl;
+                                }
+                                else 
+                                {
+                                    cout<<"TIE"<<endl;
+                                }
+                        }
+                    
+                }
+            
+        }
+    }
+    return 0;
+}
