980b043b53100672f2aa3bb346ef9c87f0f9c02d
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-03-06 13:19:44 +0530

    Solved: How much Scholarship (ZCOSCH)

---
+++ ZCOSCH-How-much-Scholarship/How-much-Scholarship.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // your code goes here
+       int n;
+       cin>>n;
+        if(n>=1 && n<=50)
+        {
+            cout<<"100"<<endl;
+        }
+        else if(n>50 && n<=100) 
+        {
+            cout<<"50"<<endl;
+        }
+        else
+        {
+            cout<<"0"<<endl;
+        }
+       return 0;
+}
